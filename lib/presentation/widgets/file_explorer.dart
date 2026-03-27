c4db2491cc706c8ef3b1d05d2eb8ce18f1350cb1
Aleksander Pres <aleksander.pres98@gmail.com>
2026-03-27 16:28:42 +0100

    open previous directory button added

--- lib/presentation/widgets/file_explorer.dart
+++ lib/presentation/widgets/file_explorer.dart
@@ -1,4 +1,7 @@
+import 'package:client/business_logic/entities/file.dart';
+import 'package:client/data/models/file.dart';
 import 'package:client/presentation/bloc/media_cloud_bloc.dart';
+import 'package:client/presentation/bloc/media_cloud_event.dart';
 import 'package:client/presentation/bloc/media_cloud_state.dart';
 import 'package:client/presentation/widgets/components/file_representation.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +12,8 @@ class FileExplorer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MediaCloudBloc mediaCloudBloc = context.read<MediaCloudBloc>();
+
     return Scaffold(
       backgroundColor: Colors.grey.shade600,
       body: BlocBuilder<MediaCloudBloc, MediaCloudState>(
@@ -18,20 +23,43 @@ class FileExplorer extends StatelessWidget {
               return CircularProgressIndicator();
             case FileExplorerStatus.success:
               if (state.loadedFiles != null) {
+                List<FileEntity> files = state.loadedFiles!;
+                if (state.currentDirectory != null) {
+                  files.insert(0, FileModel(name: 'Previous'));
+                }
                 return GridView.builder(
                   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                     maxCrossAxisExtent: 150,
                     mainAxisSpacing: 10,
                     crossAxisSpacing: 10,
                   ),
-                  itemCount: state.loadedFiles!.length,
+                  itemCount: files.length,
                   itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: FileRepresentation(
-                        file: state.loadedFiles![index],
-                      ),
-                    );
+                    if (files[index].name == 'Previous') {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                          icon: Icon(Icons.subdirectory_arrow_left),
+                          iconSize: 75,
+                          onPressed: () {
+                            if (state.currentDirectory!.parentId != null) {
+                              mediaCloudBloc.add(
+                                OpenPreviousDirectoryEvent(
+                                  state.currentDirectory!.parentId!,
+                                ),
+                              );
+                            } else {
+                              mediaCloudBloc.add(OpenDirectoryEvent(null));
+                            }
+                          },
+                        ),
+                      );
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FileRepresentation(file: files[index]),
+                      );
+                    }
                   },
                 );
               } else {
